5825fcdd164a7703dcc23162d4119cf53d54ad52
mariasosaluna <112974049+mariasosaluna@users.noreply.github.com>
2022-12-07 16:26:46 +0100

    feat: Add chakra

--- App/src/App.jsx
+++ App/src/App.jsx
@@ -1,17 +1,28 @@
-
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import FaceButton from './components/FacebookButton';
-import GoogleButton from './components/GoogleButton';
+import { Card, CardBody, Text } from "@chakra-ui/react";
+import FaceButton from "./components/FacebookButton";
+import GoogleButton from "./components/GoogleButton";
+import UiButton from "./components/UIButton";
+import UiInput from "./components/UIInput";
 
 function App() {
   return (
     <div className="App">
       <FaceButton />
       <GoogleButton />
+      <UiInput />
+      <UiButton>Example default</UiButton>
+      <UiButton onClick={() => alert("pepe")} variant="primary">
+        Example primary
+      </UiButton>
+      <UiButton variant="secondary">Example secondary</UiButton>
+
+      <Card>
+        <CardBody>
+          <Text>View a summary of all your customers over the last month.</Text>
+        </CardBody>
+      </Card>
     </div>
   );
-
 }
 
 export default App;
