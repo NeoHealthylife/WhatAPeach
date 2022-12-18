6f08213d304d716e996ba702ac3099da428d5c7a
mariasosaluna <112974049+mariasosaluna@users.noreply.github.com>
2022-12-18 19:01:57 +0100

    feat: last styles and challenges

--- App/src/pages/Dashboard/Dashboard.jsx
+++ App/src/pages/Dashboard/Dashboard.jsx
@@ -1,27 +1,51 @@
-import React, { useEffect, useState } from "react";
-import { NavLink } from "react-router-dom";
+import React, { useContext, useEffect, useState } from "react";
+import { NavLink, useNavigate } from "react-router-dom";
 import styled from "styled-components";
 import CardList from "../../components/CardList";
+import GlobalContext from "../../context/GlobalContext";
 import { API } from "../../services/API";
 
 const DashboardCards = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
+  flex-direction: column;
+  margin-bottom: 3rem;
+
+  @media (min-width: 600px) {
+    flex-direction: row;
+  }
 `;
 
-const CardStyled = styled(NavLink)`
-  width: 600px;
-  height: 380px;
-  background-size: 100%;
+const CardStyled = styled.button`
+  width: 100%;
+  min-height: 200px;
+  background-size: 120% 120%;
   background-repeat: no-repeat;
-  margin: 50px;
+  background-position: center;
   border-radius: 20px;
   display: flex;
   justify-content: center;
   align-items: center;
   color: #fe9066;
   font-size: xx-large;
+  margin-top: 2rem;
+  text-align: center;
+
+  :hover {
+    box-shadow: 3px 6px 0 #fe9166;
+    margin-top: 1.8rem;
+  }
+
+  @media (min-width: 600px) {
+    flex-direction: row;
+    margin: 50px;
+  }
+
+  @media (min-width: 1600px) {
+    width: 600px;
+    height: 380px;
+  }
 
   &.recipes {
     background-image: url("https://res.cloudinary.com/drh0lkvxh/image/upload/v1671276369/HealthyLife/recipes_utbxbb.jpg");
@@ -31,7 +55,6 @@ const CardStyled = styled(NavLink)`
   }
 `;
 const HeadingStyled = styled.div`
-  width: 500px;
   color: #384d62;
   font-size: xx-large;
   padding: 20px;
@@ -39,7 +62,6 @@ const HeadingStyled = styled.div`
   justify-content: center;
 `;
 const TextStyled = styled.div`
-  width: 500px;
   color: #384d62;
   padding: 20px;
   display: flex;
@@ -49,12 +71,20 @@ const TextStyled = styled.div`
 const CardTextStyled = styled.div`
   color: #fe9066;
   font-size: xx-large;
-  margin-top: 2rem;
+  margin: 2rem 0 1rem;
 `;
+
+const challengeHasExpired = (item, savedDate) => {
+  const difference = new Date(savedDate) - new Date();
+  const totalDays = Math.ceil(difference / (1000 * 3600 * 24));
+  return totalDays > 7;
+};
 
 const Dashboard = () => {
   const [recipes, setRecipes] = useState([]);
   const [workouts, setworkouts] = useState([]);
+  const { setItem } = useContext(GlobalContext);
+  let navigate = useNavigate();
 
   useEffect(() => {
     API.get("/recipes").then((res) => setRecipes(res.data.data.recipes));
@@ -63,6 +93,40 @@ const Dashboard = () => {
   useEffect(() => {
     API.get("/workouts").then((res) => setworkouts(res.data.data.workouts));
   }, []);
+
+  const handleRecipeClick = () => {
+    const savedRecipeString = localStorage.getItem("weekRecipe");
+    const savedRecipeDateString = localStorage.getItem("weekRecipeDate");
+    const savedRecipe = JSON.parse(savedRecipeString);
+    const hasExpired = challengeHasExpired(savedRecipe, savedRecipeDateString);
+
+    if (savedRecipe && !hasExpired) {
+      setItem(savedRecipe);
+    } else {
+      const randNum = Math.floor(Math.random() * recipes.length);
+      localStorage.setItem("weekRecipe", JSON.stringify(recipes[randNum]));
+      localStorage.setItem("weekRecipeDate", new Date());
+      setItem(recipes[randNum]);
+    }
+    navigate("/recipes/detail");
+  };
+
+  const handleWorkoutClick = () => {
+    const savedWorkoutString = localStorage.getItem("weekWorkout");
+    const savedWorkoutDateString = localStorage.getItem("weekWorkoutDate");
+    const savedWorkout = JSON.parse(savedWorkoutString);
+    const hasExpired = challengeHasExpired(savedWorkout, savedWorkoutDateString);
+
+    if (savedWorkout && !hasExpired) {
+      setItem(savedWorkout);
+    } else {
+      const randNum = Math.floor(Math.random() * workouts.length);
+      localStorage.setItem("weekWorkout", JSON.stringify(workouts[randNum]));
+      localStorage.setItem("weekWorkoutDate", new Date());
+      setItem(workouts[randNum]);
+    }
+    navigate("/workouts/detail");
+  };
 
   return (
     <>
@@ -76,11 +140,11 @@ const Dashboard = () => {
         </h2>
       </TextStyled>
       <DashboardCards>
-        <CardStyled className="recipes" to="/recipes">
-          Recetas
+        <CardStyled className="recipes" onClick={() => handleRecipeClick()}>
+          Reto semanal de recetas
         </CardStyled>
-        <CardStyled className="workouts" to="/workouts">
-          Workouts
+        <CardStyled className="workouts" onClick={() => handleWorkoutClick()}>
+          Reto semanal de workouts
         </CardStyled>
       </DashboardCards>
 
